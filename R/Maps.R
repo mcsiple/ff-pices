47520db646d3a7ee2a67ac6047b3d7fdc120ce0e
Megsie Siple <mcsiple@gmail.com>
2020-09-22 18:30:55 -0700

    fix old maps (depracated by useful anyway)

--- R/Maps.R
+++ R/Maps.R
@@ -1,38 +1,26 @@
 # Global map of LMEs and degree of asynchrony or sardine-anchovy dominance
-library(rgdal)
+#library(rgdal)
 library(leaflet)
 library(data.table)
 library(mapview)    # For saving leaflet maps as pdf or html 
-mapwd <- here::here("R/Map/LME66") # shapefile for Large Marine Ecosystems
+library(sf) # contains function for getting  polygon centroids
 
-lmes <- readOGR(file.path(mapwd,"LMEs66.shp"))
+mapwd <- here::here('data') # shapefile for Large Marine Ecosystems
 
+
+lmes <- st_read(here::here('data','LMEs66.shp')) %>%
+  st_transform(32617)
+sf_cent <- st_centroid(lmes)
+
+plot(lmes)
 # Look at the data
 head(lmes@data)
 class(lmes) # "SpatialPolygonsDataFrame"
 names(lmes)
 
-
 # Color polygons by asynchrony variable in lmes@data or otherwise
 # I got the following code from: https://github.com/rstudio/leaflet/issues/169
 lmes.dt <- data.table(lmes@data)
-
-# ***** This is just a test table with asynchrony measures - to fill polygons with colors based on asynchrony level
-asyn.test <- data.table(lme=c(3,13,21,22),
-                        region.name=c("California",
-                                      "Humboldt",
-                                      "NE Atlantic",
-                                      "NE Atlantic"),
-                        asynchrony = c(0.5,0.6,0.7,0.2))
-
-# Create an explicit attribute to keep polygons IDs (useful to "re-attach" the table to the polygons later)
-lmes.dt[, rn := row.names(lmes)]
-setkey(lmes.dt, LME_NUMBER)
-setkey(asyn.test, lme)
-lmes.dt2 <- asyn.test[lmes.dt]
-
-setkey(lmes.dt2, rn)
-lmes@data <- data.frame(lmes.dt2[row.names(lmes)])
 
 
 # Use leaflet to make map -------------------------------------------------
